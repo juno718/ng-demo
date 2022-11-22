daf04e19d3c133787e0fe1870b33e8d5127b29fd
juno718 <juno20020718@gmail.com>
2022-11-22 21:16:39 +0800

    11/22 end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { BookspageComponent } from './bookspage/bookspage.component';//路由的import
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'books',component:BookspageComponent}
+];//路由設定
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/bookspage/bookspage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookspage',
+  templateUrl: './bookspage.component.html',
+  styleUrls: ['./bookspage.component.css']
+})
+export class BookspageComponent {
+
+}
